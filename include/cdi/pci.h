d62d45d26062eae4dac83751ad5167be720b3017
svn-taljeth <svn-taljeth@1fb02b30-9e10-0410-89f7-8f5a202ca6a9>
2007-12-17 19:33:22 +0000

    + CDI: Ein paar wilde Spinnereien, wie ein gemeinsames Treiberinterface aussehen könnte
    git-svn-id: svn+ssh://overgames.de/lost/trunk@644 1fb02b30-9e10-0410-89f7-8f5a202ca6a9

---
+++ include/cdi/pci.h
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2007 Kevin Wolf
+ *
+ * This program is free software. It comes without any warranty, to
+ * the extent permitted by applicable law. You can redistribute it 
+ * and/or modify it under the terms of the Do What The Fuck You Want 
+ * To Public License, Version 2, as published by Sam Hocevar. See
+ * http://sam.zoy.org/projects/COPYING.WTFPL for more details.
+ */  
+
+#ifndef _CDI_PCI_H_
+#define _CDI_PCI_H_
+
+#include <stdint.h>
+#include <collections.h> 
+
+#include "cdi.h"
+
+struct cdi_pci_device {
+    uint16_t    vendor_id;
+    uint16_t    device_id;
+};
+
+/**
+ * Gibt alle PCI-Geraete im System zurueck. Die Geraete werden dazu
+ * in die uebergebene Liste eingefuegt.
+ */
+void cdi_pci_get_all_devices(list_t* list);
+
+/**
+ * Gibt die Information zu einem PCI-Geraet frei
+ */
+void cdi_pci_device_destroy(struct cdi_pci_device* device);
+
+#endif
+
---
+++ sis900/main.c
@@ -0,0 +1,122 @@
+/*  
+ * Copyright (c) 2007 The LOST Project. All rights reserved.
+ *
+ * This code is derived from software contributed to the LOST Project
+ * by Kevin Wolf.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *     This product includes software developed by the LOST Project
+ *     and its contributors.
+ * 4. Neither the name of the LOST Project nor the names of its
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR 
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, 
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, 
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; 
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, 
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR 
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF 
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <stdint.h>
+#include <stdlib.h>
+
+#include "cdi/net.h"
+#include "cdi/pci.h"
+
+#include "device.h"
+
+struct sis900_driver {
+    struct cdi_net_driver net;
+};
+
+static struct sis900_driver sis900_driver;
+
+
+static void sis900_driver_init(struct sis900_driver* driver);
+static void sis900_driver_destroy(struct sis900_driver* driver);
+static void sis900_device_init(
+    struct sis900_device* device, struct cdi_pci_device* pcidev);
+
+#ifdef CDI_STANDALONE
+int main()
+#else
+int init_sis900
+#endif
+{
+    cdi_init();
+
+    sis900_driver_init(&sis900_driver);
+    cdi_driver_register((struct cdi_driver*) &sis900_driver);
+
+#ifdef CDI_STANDALONE
+    cdi_run_drivers();
+#endif    
+
+    sis900_driver_destroy(&sis900_driver);
+
+    return 0;
+}
+
+/**
+ * Initialisiert die Datenstrukturen fuer den sis900-Treiber
+ */
+static void sis900_driver_init(struct sis900_driver* driver)
+{
+    // Konstruktor der Vaterklasse
+    cdi_net_driver_init((struct cdi_net_driver*) driver);
+
+    // Funktionspointer initialisieren
+    driver->net.drv.init_device     = sis900_init_device;
+    driver->net.drv.remove_device   = sis900_remove_device;
+    driver->net.send_packet         = sis900_send_packet;
+
+    // Passende PCI-Geraete suchen
+    list_t* pci_devices = list_create();
+    cdi_pci_get_all_devices(pci_devices);
+
+    struct cdi_pci_device* dev;
+    int i;
+    for (i = 0; (dev = list_get_element_at(pci_devices, i)); i++) {
+        if ((dev->vendor_id == 0x1039) && (dev->device_id == 0x0900)) {
+            struct sis900_device* device = malloc(sizeof(*device));
+            sis900_device_init(device, dev);
+        } else {
+            cdi_pci_device_destroy(dev);
+        }
+    }
+
+    list_destroy(pci_devices);
+}
+
+/**
+ * Deinitialisiert die Datenstrukturen fuer den sis900-Treiber
+ */
+static void sis900_driver_destroy(struct sis900_driver* driver)
+{
+    cdi_net_driver_destroy((struct cdi_net_driver*) driver);
+}
+
+/**
+ * Initialisiert die Datenstrukturen fuer eine sis900-Karte
+ */
+static void sis900_device_init(
+    struct sis900_device* device, struct cdi_pci_device* pcidev)
+{
+    device->pci = pcidev;
+}
