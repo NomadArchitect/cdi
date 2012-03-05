NomadArchitect/cdi

commit 10f8c755966d1cfa472a4f07202fa5cb3f67f2ac

Move complete initialisation to device_init
* Up to now, the cdi-device struct was created during the init_driver
  call of the driver and init_device only completed the initialisation.
  The new model is that init_device gets bus information (e.g. PCI
  address, device/vendor ID) and decides if it can handle the device. If
  so, it creates a cdi_device object and returns it.

  This way device creation is decoupled from driver initialisation and
  hot-plugging becomes possible.

Signed-off-by: Kevin Wolf <kevin@tyndur.org>
Acked-by: Max Reitz <max@tyndur.org>
Acked-by: Matthew Iselin <matthew@theiselins.net>

floppy/main.c
@@ -68,6 +68,7 @@ static int floppy_driver_init(void)
 
         // Geraet nur eintragen wenn es existiert
         if (floppy_device_probe(device) != 0) {
+            floppy_init_device((struct cdi_device*) device);
             cdi_list_push(floppy_driver.drv.devices, device);
         } else {
             free(device);
@@ -107,7 +108,6 @@ static struct cdi_storage_driver floppy_driver = {
         .name           = DRIVER_NAME,
         .init           = floppy_driver_init,
         .destroy        = floppy_driver_destroy,
-        .init_device    = floppy_init_device,
         .remove_device  = floppy_remove_device,
     },
     .read_blocks        = floppy_read_blocks,

ata/main.c
@@ -181,7 +181,6 @@ static struct cdi_storage_driver driver_storage = {
         .name           = DRIVER_STORAGE_NAME,
         .init           = ata_driver_init,
         .destroy        = ata_driver_destroy,
-        .init_device    = ata_init_device,
         .remove_device  = ata_remove_device,
     },
     .read_blocks        = ata_read_blocks,
@@ -194,7 +193,6 @@ static struct cdi_scsi_driver driver_scsi = {
         .name           = DRIVER_SCSI_NAME,
         .init           = atapi_driver_init,
         .destroy        = atapi_driver_destroy,
-        .init_device    = atapi_init_device,
         .remove_device  = atapi_remove_device,
     },
     .request            = atapi_request,

e1000/main.c
@@ -53,19 +53,13 @@ static int e1000_driver_init(void)
     cdi_list_t pci_devices = cdi_list_create();
     cdi_pci_get_all_devices(pci_devices);
 
+    struct cdi_device* device;
     struct cdi_pci_device* dev;
     int i;
     for (i = 0; (dev = cdi_list_get(pci_devices, i)); i++) {
-        if ((dev->vendor_id == 0x8086) && (dev->device_id == 0x100e)) {
-            void* phys_device;
-            struct e1000_device* device;
-
-            cdi_alloc_phys_mem(sizeof(*device),
-                (void**) &device, &phys_device);
-            memset(device, 0, sizeof(*device));
-
-            device->phys = phys_device;
-            device->net.dev.bus_data = (struct cdi_bus_data*) dev;
+        device = e1000_init_device((struct cdi_bus_data*) dev);
+        if (device != NULL) {
+            // TODO
             cdi_list_push(driver.drv.devices, device);
         } else {
             cdi_pci_device_destroy(dev);
@@ -95,8 +89,9 @@ static int e1000_driver_destroy(void)
 
 static struct cdi_net_driver driver = {
     .drv = {
-        .type           = CDI_NETWORK,
         .name           = DRIVER_NAME,
+        .type           = CDI_NETWORK,
+        .bus            = CDI_PCI,
         .init           = e1000_driver_init,
         .destroy        = e1000_driver_destroy,
         .init_device    = e1000_init_device,

ne2k/main.c
@@ -49,18 +49,11 @@ static int ne2k_driver_init(void)
     cdi_pci_get_all_devices(pci_devices);
 
     struct cdi_pci_device* dev;
+    struct cdi_device* device;
     int i;
     for (i = 0; (dev = cdi_list_get(pci_devices, i)); i++) {
-        if ((dev->vendor_id == 0x10ec) && (dev->device_id == 0x8029)) {
-            void* phys_device;
-            struct ne2k_device* device;
-
-            cdi_alloc_phys_mem(sizeof(*device),
-                (void**) &device, &phys_device);
-            memset(device, 0, sizeof(*device));
-
-            device->phys = phys_device;
-            device->net.dev.bus_data = (struct cdi_bus_data*) dev;
+        device = ne2k_init_device((struct cdi_bus_data*) dev);
+        if (device != NULL) {
             cdi_list_push(ne2k_driver.drv.devices, device);
         } else {
             cdi_pci_device_destroy(dev);
@@ -87,8 +80,9 @@ static int ne2k_driver_destroy(void)
 
 static struct cdi_net_driver ne2k_driver = {
     .drv = {
-        .type           = CDI_NETWORK,
         .name           = DRIVER_NAME,
+        .type           = CDI_NETWORK,
+        .bus            = CDI_PCI,
         .init           = ne2k_driver_init,
         .destroy        = ne2k_driver_destroy,
         .init_device    = ne2k_init_device,

pcnet/main.c
@@ -53,15 +53,11 @@ static int pcnet_driver_init(void)
     cdi_pci_get_all_devices(pci_devices);
 
     struct cdi_pci_device* dev;
+    struct cdi_device* device;
     int i;
     for (i = 0; (dev = cdi_list_get(pci_devices, i)); i++) {
-        if ((dev->vendor_id == VENDOR_ID) && (dev->device_id == DEVICE_ID)) {
-            struct pcnet_device* device;
-            device = malloc(sizeof(struct pcnet_device));
-
-            memset(device, 0, sizeof(struct pcnet_device));
-
-            device->net.dev.bus_data = (struct cdi_bus_data*) dev;
+        device = pcnet_init_device((struct cdi_bus_data*) dev);
+        if (device) {
             cdi_list_push(pcnet_driver.drv.devices, device);
         } else {
             cdi_pci_device_destroy(dev);
@@ -88,8 +84,9 @@ static int pcnet_driver_destroy(void)
 
 static struct cdi_net_driver pcnet_driver = {
     .drv = {
-        .type           = CDI_NETWORK,
         .name           = DRIVER_NAME,
+        .type           = CDI_NETWORK,
+        .bus            = CDI_PCI,
         .init           = pcnet_driver_init,
         .destroy        = pcnet_driver_destroy,
         .init_device    = pcnet_init_device,

rtl8139/main.c
@@ -52,18 +52,11 @@ static int rtl8139_driver_init(void)
     cdi_pci_get_all_devices(pci_devices);
 
     struct cdi_pci_device* dev;
+    struct cdi_device* device;
     int i;
     for (i = 0; (dev = cdi_list_get(pci_devices, i)); i++) {
-        if ((dev->vendor_id == 0x10ec) && (dev->device_id == 0x8139)) {
-            void* phys_device;
-            struct rtl8139_device* device;
-
-            cdi_alloc_phys_mem(sizeof(*device),
-                (void**) &device, &phys_device);
-            memset(device, 0, sizeof(*device));
-
-            device->phys = phys_device;
-            device->net.dev.bus_data = (struct cdi_bus_data*) dev;
+        device = rtl8139_init_device((struct cdi_bus_data*) dev);
+        if (device != NULL) {
             cdi_list_push(rtl8139_driver.drv.devices, device);
         } else {
             cdi_pci_device_destroy(dev);
@@ -90,8 +83,9 @@ static int rtl8139_driver_destroy(void)
 
 static struct cdi_net_driver rtl8139_driver = {
     .drv = {
-        .type           = CDI_NETWORK,
         .name           = DRIVER_NAME,
+        .type           = CDI_NETWORK,
+        .bus            = CDI_PCI,
         .init           = rtl8139_driver_init,
         .destroy        = rtl8139_driver_destroy,
         .init_device    = rtl8139_init_device,

sis900/main.c
@@ -54,18 +54,11 @@ static int sis900_driver_init(void)
     cdi_pci_get_all_devices(pci_devices);
 
     struct cdi_pci_device* dev;
+    struct cdi_device* device;
     int i;
     for (i = 0; (dev = cdi_list_get(pci_devices, i)); i++) {
-        if ((dev->vendor_id == 0x1039) && (dev->device_id == 0x0900)) {
-            void* phys_device;
-            struct sis900_device* device;
-            
-            cdi_alloc_phys_mem(sizeof(*device), 
-                (void**) &device, &phys_device);
-            memset(device, 0, sizeof(*device));
-
-            device->phys = phys_device;
-            device->net.dev.bus_data = (struct cdi_bus_data*) dev;
+        device = sis900_init_device((struct cdi_bus_data*) dev);
+        if (device != NULL) {
             cdi_list_push(sis900_driver.drv.devices, device);
         } else {
             cdi_pci_device_destroy(dev);
@@ -95,8 +88,9 @@ static int sis900_driver_destroy(void)
 
 static struct cdi_net_driver sis900_driver = {
     .drv = {
-        .type           = CDI_NETWORK,
         .name           = DRIVER_NAME,
+        .type           = CDI_NETWORK,
+        .bus            = CDI_PCI,
         .init           = sis900_driver_init,
         .destroy        = sis900_driver_destroy,
         .init_device    = sis900_init_device,

ata/atapi.c
@@ -77,7 +77,7 @@ int atapi_drv_identify(struct ata_device* dev)
     return 1;
 }
 
-void atapi_init_device(struct cdi_device* device)
+void atapi_init_device(struct ata_device* device)
 {
     struct cdi_scsi_device* scsi = (struct cdi_scsi_device*) device;
 

commit 3a6ddc9a339ce13e28d48e2bafeb8de9f943857d

floppy: Fix initialisation
Commit 10f8c755 broke the floppy driver by initialising the drives
before the controller. This fix restores the right order.

Additionally, floppy must assign the device's driver after 10f8c755
because it calls the device init function before generic CDI code can do
the job. This could clearly need some cleanup so that CDI calls the
device init functions again, but for now let's just fix the breakage.

Signed-off-by: Kevin Wolf <kevin@tyndur.org>

floppy/main.c
@@ -68,7 +68,7 @@ static int floppy_driver_init(void)
 
         // Geraet nur eintragen wenn es existiert
         if (floppy_device_probe(device) != 0) {
-            floppy_init_device((struct cdi_device*) device);
+            device->dev.dev.driver = &floppy_driver.drv;
             cdi_list_push(floppy_driver.drv.devices, device);
         } else {
             free(device);
@@ -84,6 +84,10 @@ static int floppy_driver_init(void)
     // Treiber gleich wieder beendet werden
     if (floppy_init_controller(&floppy_controller) != 0) {
         return -1;
+    }
+
+    for (i = 0; (device = cdi_list_get(floppy_driver.drv.devices, i)); i++) {
+        floppy_init_device(&device->dev.dev);
     }
 
     return 0;